be628851b14986dc02adf349515583a77853860f
lightningsingh <53794326+lightningsingh@users.noreply.github.com>
2025-02-16 10:44:02 +0000

    Fixed Next.js setup

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import React from "react";
-import Home from "../src/components/Home";
-
-export default function Index() {
-  return <Home />;
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Lightning Singh's Website</h1>
+      <p>This is a placeholder while we build the full site.</p>
+    </div>
+  );
 }
