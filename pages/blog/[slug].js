1a074cc7945910d6b192ef0e4b96556c7490f341
lightningsingh <53794326+lightningsingh@users.noreply.github.com>
2025-02-20 18:42:37 +0000

    Added blog functionality

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -3,6 +3,10 @@ import { remark } from "remark";
 import html from "remark-html";
 
 export default function BlogPost({ post }) {
+  if (!post) {
+    return <h1 className="text-center text-red-600">Post not found</h1>;
+  }
+
   return (
     <div className="max-w-3xl mx-auto py-10">
       <h1 className="text-3xl font-bold">{post.title}</h1>
@@ -26,6 +30,10 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   const postData = getPostData(params.slug);
+  if (!postData) {
+    return { notFound: true };
+  }
+
   const processedContent = await remark().use(html).process(postData.content);
   
   return {
